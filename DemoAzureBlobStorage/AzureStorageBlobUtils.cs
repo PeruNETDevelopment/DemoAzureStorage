1505ddb075b23ab08d8d1b0ab96b685ece06bb7c
Erick Orlando <erickorlando@outlook.com>
2017-08-08 17:30:19 -0500

    Agregar archivos de proyecto.

---
+++ DemoAzureBlobStorage/AzureStorageBlobUtils.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace DemoAzureBlobStorage
+{
+    public class AzureStorageBlobUtils
+    {
+        private const string ContainerName = "meetupagosto";
+
+        private static CloudBlobClient GetBlobClient()
+        {
+            // Utilizamos la cuenta de Azure para extraerlo del BLOB.
+            var account = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StoreSettings"));
+            // Creamos el cliente.
+            return account.CreateCloudBlobClient();
+        }
+
+        public static async void UploadDocumentToAzure(UploadDocumentAzureRequest request)
+        {
+            var blobClient = GetBlobClient();
+
+            // Ubicamos el archivo creando la referencia al Contenedor.
+            var container = blobClient.GetContainerReference(ContainerName);
+
+            // Si no existe lo creamos.
+            await container.CreateIfNotExistsAsync();
+            var blob = container.GetBlockBlobReference(request.NombreReferencia);
+
+            await blob.UploadFromFileAsync(request.NombreArchivo);
+        }
+
+        public static async Task<string> DownloadDocument(string blobReference)
+        {
+            var blobClient = GetBlobClient();
+
+            // Ubicamos el archivo creando la referencia al Contenedor.
+            var container = blobClient.GetContainerReference(ContainerName);
+
+            if (!container.Exists())
+                throw new InvalidOperationException($"No se encuentra el contenedor {container}");
+
+            var archivo = container.GetBlockBlobReference(blobReference);
+
+            using (var memArchivo = new MemoryStream())
+            {
+                await archivo.DownloadToStreamAsync(memArchivo);
+
+                return Convert.ToBase64String(memArchivo.ToArray());
+            }
+        }
+    }
+
+    public class UploadDocumentAzureRequest
+    {
+        public string NombreReferencia { get; set; }
+        public string NombreArchivo { get; set; }
+    }
+}
